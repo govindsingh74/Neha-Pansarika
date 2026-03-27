6bee4dd5ac714a329ba4f97c019c35b52c2b82f4
govindsingh74 <143626813+govindsingh74@users.noreply.github.com>
2026-03-27 12:46:33 +0530

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Header } from './components/common/Header';
 import { Footer } from './components/common/Footer';
 import { Home } from './pages/Home';
